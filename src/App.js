5d299a754ff3b03f9f44ca00c46f6b6ea9c5d84a
Yilika Loufoua <59208173+YilikaLoufoua@users.noreply.github.com>
2022-07-01 18:24:24 -0700

    Finished preliminary prediction testing with real categories and unseen test data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-tabs */
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { CSVLink } from 'react-csv';
 import Papa from 'papaparse';
@@ -13,287 +11,263 @@ import DataTable from './Components/DataTable/DataTable';
 const brain = require('brain.js'); // import from is not supported by brain.js
 
 function App() {
-  const [file, setFile] = useState(); // CSV file to be uploaded
-  const [data, setData] = useState(); // parsed data as json object from CSV file
-  const [networkJson, setNetworkJson] = useState(undefined); // trained neural network
-  const [selectedColumn, setSelectedColumn] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [glowFeedbacks, setGlowFeedbacks] = useState([]);
-  const [growFeedbacks, setGrowFeedbacks] = useState([]);
-  const [categorization, setCategorization] = useState({});
-  const [csvData, setCSVData] = useState([]);
-  // console.log('glow: ', glowFeedbacks);
-  // console.log('grow: ', growFeedbacks);
-  // console.log('file: ', file);
-  // console.log('data: ', data);
-  // console.log('network: ', networkJson);
-  // console.log('column: ', selectedColumn);
-  // console.log('categorization: ', categorization);
-  // console.log('csvData: ', csvData);
-  // console.log('category: ', selectedCategory);
+	const [file, setFile] = useState(); // CSV file to be uploaded
+	const [data, setData] = useState(); // parsed data as json object from CSV file
+	const [networkJson, setNetworkJson] = useState(undefined); // trained neural network
+	const [selectedColumn, setSelectedColumn] = useState('');
+	const [selectedCategory, setSelectedCategory] = useState('');
+	const [glowFeedbacks, setGlowFeedbacks] = useState([]);
+	const [growFeedbacks, setGrowFeedbacks] = useState([]);
+	const [categorization, setCategorization] = useState({});
 
-  // Create a dictionary of categories and feedbacks
-  const addGlow = (item) => {
-    if (glowFeedbacks.includes(item)) return;
-    if (growFeedbacks.includes(item)) {
-      setGrowFeedbacks(growFeedbacks.filter((item2) => item2 != item));
-    }
-    setGlowFeedbacks((prev) => [...prev, item]);
-  };
-  const addGrow = (item) => {
-    if (growFeedbacks.includes(item)) return;
-    if (glowFeedbacks.includes(item)) {
-      setGlowFeedbacks(glowFeedbacks.filter((item2) => item2 != item));
-    }
-    setGrowFeedbacks((prev) => [...prev, item]);
-  };
-  const categories = [
-    'Course Experience',
-    'Material',
-    'Lecture',
-    'Course Structure',
-    'Peer Relations',
-    'Administration',
-    'Support',
-    'Technical Topic',
-  ];
+	const [csvData, setCSVData] = useState([]);
+	const [trainingData, setTrainingData] = useState([]);
 
-  // Process parsed CSV
-  //   useEffect(() => {
-  //     if (data) {
-  //       processData();
-  //     }
-  //   }, [data]);
+	useEffect(() => {
+		const obj = {};
+		categories.forEach((category) => {
+			obj[category] = [];
+		});
+		setCategorization(obj);
+	}, [data]);
 
-  // Format categorization data for export
-  useEffect(() => {
-    const result = Object.entries(categorization).reduce(
-      (previousValue, category) => {
-        const key = category[0];
-        const subArray = category[1];
-        // If there is a value, then add the value to the array
-        if (subArray.length) {
-          // Do something
-          const convertedArary = subArray.map((value) => ({ [key]: value }));
+	useEffect(() => {
+		// Format categorization data for export
+		const result = Object.entries(categorization).reduce((previousValue, category) => {
+			const key = category[0];
+			const subArray = category[1];
+			// If there is a value, then add the value to the array
+			if (subArray.length) {
+				// Do something
+				const convertedArary = subArray.map((value) => ({ [key]: value }));
+				return [...previousValue, ...convertedArary];
+			}
+			return previousValue;
+		}, []);
+		setCSVData(result);
 
-          return [...previousValue, ...convertedArary];
-        }
+		// Format categorization data for generating neural network training data
+		Object.keys(categorization).forEach((key) => {
+			if (categorization[key].length > 0) {
+				for (let value of categorization[key]) {
+					let obj = {};
+					obj.input = value;
+					obj.output = key;
+					setTrainingData((prev) => [...prev, obj]);
+				}
+			}
+		});
+	}, [categorization]);
 
-        return previousValue;
-      },
-      []
-    );
+	// Upload CSV file
+	const fileReader = new FileReader();
+	const handleOnChange = (e) => {
+		setFile(e.target.files[0]);
+	};
 
-    setCSVData(result);
+	const displayCSV = (e) => {
+		e.preventDefault();
 
-    // Object.keys(categorization).forEach((category) => {
+		if (file) {
+			fileReader.onload = function (event) {
+				const csvString = event.target.result;
+				const data = Papa.parse(csvString).data;
+				setData(data);
+			};
+			fileReader.readAsText(file);
+		}
+	};
 
-    // 	categorization[category].forEach((feedback) => {
-    // 		let obj = {};
-    // 		obj[category] = feedback;
-    // 		csvData.push(obj);
-    //
+	const categories = [
+		'Course Experience',
+		'Material',
+		'Lecture',
+		'Course Structure',
+		'Peer Relations',
+		'Administration',
+		'Support',
+		'Technical Topic',
+	];
 
-    // 		setCSVData(csvData);
-    // 	});
-    // });
-    // categorization[selectedCategory].forEach((feedback) => {
-    // 	let obj = {};
-    // 	obj[selectedCategory] = feedback;
-    // 	csvExport.push(obj);
-    // });
-  }, [categorization]);
+	const handleAddCategory = (category, feedback) => {
+		let categorizedFeedbacks = categorization[category];
 
-  const handleAddCategory = (category, feedback) => {
-    let categorizedFeedbacks = categorization[category];
+		if (
+			categorizedFeedbacks.find((item) => {
+				return feedback === item;
+			})
+		)
+			return;
+		categorizedFeedbacks.push(feedback);
 
-    if (
-      categorizedFeedbacks.find((item) => {
-        return feedback === item;
-      })
-    )
-      return;
-    categorizedFeedbacks.push(feedback);
+		let newCategorization = {};
+		newCategorization[category] = categorizedFeedbacks;
 
-    let newCategorization = {};
-    newCategorization[category] = categorizedFeedbacks;
+		setCategorization((prevState) => ({
+			...prevState,
+			...newCategorization,
+		}));
+	};
 
-    setCategorization((prevState) => ({
-      ...prevState,
-      ...newCategorization,
-    }));
-  };
+	const addGlow = (item) => {
+		if (glowFeedbacks.includes(item)) return;
+		if (growFeedbacks.includes(item)) {
+			setGrowFeedbacks(growFeedbacks.filter((item2) => item2 != item));
+		}
+		setGlowFeedbacks((prev) => [...prev, item]);
+	};
+	const addGrow = (item) => {
+		if (growFeedbacks.includes(item)) return;
+		if (glowFeedbacks.includes(item)) {
+			setGlowFeedbacks(glowFeedbacks.filter((item2) => item2 != item));
+		}
+		setGrowFeedbacks((prev) => [...prev, item]);
+	};
 
-  const processData = () => {
-    // // // Sample Network Training Process
-    // const network = new brain.recurrent.LSTM(); // Init network
-    // // Import data
-    // const hardware_software = require("./data/hardware-software.json");
-    // // Preprocess data
-    // const trainingData = hardware_software.map((item) => ({
-    //   input: item.text,
-    //   output: item.category,
-    // }));
-    // // Train and store network json object as state
-    // network.train(trainingData, {
-    //   iterations: 10, // very fast but very poor performance
-    // });
-    // setNetworkJson(network.toJSON());
-    // // INSTEAD OF TRAINING: Init new network and load from previous network
-    // const newNetwork = new brain.recurrent.LSTM();
-    // const preTrainedNetworkJson = require("./models/model.json");
-    // if (preTrainedNetworkJson) {
-    //   newNetwork.fromJSON(preTrainedNetworkJson);
-    // }
-    // // Predict
-    // const testString = "I like PC.";
-    // const output = newNetwork.run(testString);
-    //
-    //
-    //
-    // TODO: 1. Define categories for grouping feedbacks
-    // 2. Create training data with categorized and non-category feedbacks
-    // 3. Train and export network
-    // 4. Make predictions on and label each feedback
-    // 5. Create a calibration system for miscategorized feedbacks
-    // 6. Repeat retraining and evaluating until categorization decently accurate
-    // 7. Test on new dataset
-  };
+	const exportTrainingData = () => {
+		if (trainingData) {
+			const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+				JSON.stringify(trainingData)
+			)}`;
+			const link = document.createElement('a');
+			link.href = jsonString;
+			link.download = 'trainingData.json';
+			link.click();
+		}
+	};
 
-  //  TODO: Upload neural netowrk as json
-  // const importNetwork = () => {
-  // 	if (networkJson) {
-  // 		const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(JSON.stringify(networkJson))}`;
-  // 		const link = document.createElement("a");
-  // 		link.href = jsonString;
-  // 		link.download = "model.json";
-  // 		link.click();
-  // 	}
-  // };
-  useEffect(() => {
-    const obj = {};
-    categories.forEach((category) => {
-      obj[category] = [];
-    });
-    setCategorization(obj);
-    if (data) {
-      processData();
-    }
-  }, [data]);
-  // Download importable neural network as json
-  const exportNetwork = () => {
-    if (networkJson) {
-      const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
-        JSON.stringify(networkJson)
-      )}`;
-      const link = document.createElement('a');
-      link.href = jsonString;
-      link.download = 'model.json';
-      link.click();
-    }
-  };
+	const trainNetwork = () => {
+		// // Sample Network Training Process
+		// const network = new brain.recurrent.LSTM(); // Init network
+		// Train and store network json object as state
+		// network.train(trainingData, {
+		// 	iterations: 100,
+		// 	log: true,
+		// });
+		// setNetworkJson(network.toJSON());
 
-  // Upload CSV file
-  const fileReader = new FileReader();
-  const handleOnChange = (e) => {
-    console.log(e.target.file);
-    setFile(e.target.files[0]);
-  };
+		// INSTEAD OF TRAINING: Init new network and load from previous network
+		const network = new brain.recurrent.LSTM();
+		const preTrainedNetworkJson = require('./models/model.json');
+		if (preTrainedNetworkJson) {
+			network.fromJSON(preTrainedNetworkJson);
+		}
+		// Predict
+		const test1 = 'I liked the project';
+		let output = network.run(test1);
+		console.log('test1: ', test1);
+		console.log('output: ', output);
+		let test2 = 'I liked working in groups';
+		output = network.run(test2);
+		console.log('test2: ', test2);
+		console.log('output: ', output);
+		let test3 = 'I liked working on the project in groups';
+		output = network.run(test3);
+		console.log('test3: ', test3);
+		console.log('output: ', output);
+	};
 
-  // Parse CSV file
-  const displayCSV = (e) => {
-    e.preventDefault();
+	//  TODO: Upload neural netowrk as json
+	// const importNetwork = () => {
+	// 	if (networkJson) {
+	// 		const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(JSON.stringify(networkJson))}`;
+	// 		const link = document.createElement("a");
+	// 		link.href = jsonString;
+	// 		link.download = "model.json";
+	// 		link.click();
+	// 	}
+	// };
 
-    if (file) {
-      fileReader.onload = function (event) {
-        const csvString = event.target.result;
-        const data = Papa.parse(csvString).data;
-        setData(data);
-      };
-      fileReader.readAsText(file);
-    }
-  };
+	// Download importable neural network as json
+	const exportNetwork = () => {
+		if (networkJson) {
+			const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+				JSON.stringify(networkJson)
+			)}`;
+			const link = document.createElement('a');
+			link.href = jsonString;
+			link.download = 'model.json';
+			link.click();
+		}
+	};
 
-  const handleOnClear = () => {
-    setData(undefined);
-    setSelectedCategory(undefined);
-  };
+	const handleOnClear = () => {
+		setData(undefined);
+		setSelectedCategory(undefined);
+	};
+	return (
+		<>
+			<div className='header'>
+				<h1 className='title'>Feedback Analyzer</h1>
+			</div>
+			<CsvController
+				handleOnChange={handleOnChange}
+				handleOnClear={handleOnClear}
+				displayCSV={displayCSV}
+				csvData={csvData}
+				trainNetwork={trainNetwork}
+			/>
+			<CategoriesView
+				glowFeedbacks={glowFeedbacks}
+				growFeedbacks={growFeedbacks}
+				categorization={categorization}
+				selectedCategory={selectedCategory}
+				setSelectedCategory={setSelectedCategory}
+			/>
+			<div className='network-input'>
+				<button type='button'>Import Network</button>
+				<button type='button' onClick={() => exportNetwork()}>
+					Export Network
+				</button>
+			</div>
 
-  return (
-    <>
-      <div className="header">
-        <h1 className="title">Feedback Analyzer</h1>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <CsvController
-                handleOnChange={handleOnChange}
-                handleOnClear={handleOnClear}
-                displayCSV={displayCSV}
-                csvData={csvData}
-              />
-              <CategoriesView
-                glowFeedbacks={glowFeedbacks}
-                growFeedbacks={growFeedbacks}
-                categorization={categorization}
-                selectedCategory={selectedCategory}
-                setSelectedCategory={setSelectedCategory}
-              />
-              <div className="network-input">
-                <button type="button">Import Network</button>
-                <button type="button" onClick={() => exportNetwork()}>
-                  Export Network
-                </button>
-              </div>
-
-              {data && selectedColumn && (
-                <CategorizationBox
-                  setSelectedColumn={setSelectedColumn}
-                  selectedColumn={selectedColumn}
-                  data={data}
-                  categorization={categorization}
-                  glowFeedbacks={glowFeedbacks}
-                  setGlowFeedbacks={setGlowFeedbacks}
-                  addGrow={addGrow}
-                  addGlow={addGlow}
-                  handleAddCategory={handleAddCategory}
-                />
-              )}
-              <DataTable
-                data={data}
-                file={file}
-                setSelectedColumn={setSelectedColumn}
-              />
-            </>
-          }
-        />
-      </Routes>
-    </>
-  );
+			{data && selectedColumn && (
+				<CategorizationBox
+					setSelectedColumn={setSelectedColumn}
+					selectedColumn={selectedColumn}
+					data={data}
+					categorization={categorization}
+					glowFeedbacks={glowFeedbacks}
+					setGlowFeedbacks={setGlowFeedbacks}
+					addGrow={addGrow}
+					addGlow={addGlow}
+					handleAddCategory={handleAddCategory}
+					exportTrainingData={exportTrainingData}
+				/>
+			)}
+			<DataTable data={data} file={file} setSelectedColumn={setSelectedColumn} />
+		</>
+	);
 }
 
-function CsvController({ handleOnChange, displayCSV, handleOnClear, csvData }) {
-  return (
-    <div className="csv-field">
-      <h2>Import a CSV file to begin analysis</h2>
-      <div className="csv-actions">
-        <input
-          className="custom-file-input"
-          type="file"
-          id="csvFileInput"
-          accept=".csv"
-          onChange={handleOnChange}
-        />
+function CsvController({
+	handleOnChange,
+	displayCSV,
+	handleOnClear,
+	csvData,
+	trainNetwork,
+	exportTrainingData,
+}) {
+	return (
+		<div className='csv-field'>
+			<h2>Import a CSV file to begin analysis</h2>
+			<div className='csv-actions'>
+				<input
+					className='custom-file-input'
+					type='file'
+					id='csvFileInput'
+					accept='.csv'
+					onChange={handleOnChange}
+				/>
 
-        <button onClick={displayCSV}>Import CSV</button>
-        <button onClick={handleOnClear}>Clear Data</button>
-        <CSVLink data={csvData}>Export CSV</CSVLink>
-      </div>
-    </div>
-  );
+				<button onClick={displayCSV}>Import CSV</button>
+				<button onClick={handleOnClear}>Clear Data</button>
+				<CSVLink data={csvData}>Export CSV</CSVLink>
+				<button onClick={trainNetwork}>Train Network</button>
+				<button onClick={exportTrainingData}>Export Training Data</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Components/CategoriesView/CategoriesView.jsx
+++ src/Components/CategoriesView/CategoriesView.jsx
@@ -2,94 +2,76 @@ import './CategoriesView.css';
 import { useState } from 'react';
 
 export default function CategoriesView({
-  categorization,
-  selectedCategory,
-  setSelectedCategory,
-  glowFeedbacks,
-  growFeedbacks,
+	categorization,
+	selectedCategory,
+	setSelectedCategory,
+	glowFeedbacks,
+	growFeedbacks,
 }) {
-  const [selectedSentiment, setSelectedSentiment] = useState('');
-  console.log(categorization);
-  console.log(selectedCategory);
-  return (
-    <div className="categories-view">
-      <div className="category-buttons-field">
-        <div className="category-buttons">
-          {Object.keys(categorization).map((category) => {
-            if (categorization[category].length > 0) {
-              return (
-                <button
-                  className={
-                    selectedCategory === category
-                      ? 'category-button'
-                      : 'active-category-button'
-                  }
-                  key={category}
-                  onClick={() => setSelectedCategory(category)}
-                >
-                  {category}
-                </button>
-              );
-            }
-          })}
-        </div>
-        <div className="sentiment-buttons">
-          <span>Filter By: </span>
-          <button
-            className="glow-button"
-            onClick={() => setSelectedSentiment('GLOW')}
-          >
-            GLOW
-          </button>
-          <button
-            className="grow-button"
-            onClick={() => setSelectedSentiment('GROW')}
-          >
-            GROW
-          </button>
-        </div>
-      </div>
-      <CategoryView
-        category={selectedCategory}
-        feedbacks={categorization[selectedCategory]}
-        glowFeedbacks={glowFeedbacks}
-        growFeedbacks={growFeedbacks}
-        selectedSentiment={selectedSentiment}
-      />
-    </div>
-  );
+	const [selectedSentiment, setSelectedSentiment] = useState('');
+
+	return (
+		<div className='categories-view'>
+			<div className='category-buttons-field'>
+				<div className='category-buttons'>
+					{Object.keys(categorization).map((category) => {
+						if (categorization[category].length > 0) {
+							return (
+								<button
+									className={
+										selectedCategory === category ? 'category-button' : 'active-category-button'
+									}
+									key={category}
+									onClick={() => setSelectedCategory(category)}
+								>
+									{category}
+								</button>
+							);
+						}
+					})}
+				</div>
+				<div className='sentiment-buttons'>
+					<span>Filter By: </span>
+					<button className='glow-button' onClick={() => setSelectedSentiment('GLOW')}>
+						GLOW
+					</button>
+					<button className='grow-button' onClick={() => setSelectedSentiment('GROW')}>
+						GROW
+					</button>
+				</div>
+			</div>
+			<CategoryView
+				category={selectedCategory}
+				feedbacks={categorization[selectedCategory]}
+				glowFeedbacks={glowFeedbacks}
+				growFeedbacks={growFeedbacks}
+				selectedSentiment={selectedSentiment}
+			/>
+		</div>
+	);
 }
 
-export function CategoryView({
-  feedbacks,
-  selectedSentiment,
-  glowFeedbacks,
-  growFeedbacks,
-}) {
-  console.log('glowFeedbacks: ', glowFeedbacks);
-  console.log('growfeedbacks: ', growFeedbacks);
-  console.log('selectedSentiment: ', selectedSentiment);
-  console.log('feedbacks: ', feedbacks);
-  let dumbBool = true;
-  let filtered = [...growFeedbacks, ...glowFeedbacks];
-  if (feedbacks) {
-    filtered = feedbacks;
-    dumbBool = false;
-  }
-  if (selectedSentiment.length > 0) {
-    dumbBool = false;
-    filtered =
-      selectedSentiment == 'GROW'
-        ? filtered.filter((item) => growFeedbacks.includes(item))
-        : filtered.filter((item) => glowFeedbacks.includes(item));
-  }
+export function CategoryView({ feedbacks, selectedSentiment, glowFeedbacks, growFeedbacks }) {
+	let dumbBool = true;
+	let filtered = [...growFeedbacks, ...glowFeedbacks];
+	if (feedbacks) {
+		filtered = feedbacks;
+		dumbBool = false;
+	}
+	if (selectedSentiment.length > 0) {
+		dumbBool = false;
+		filtered =
+			selectedSentiment == 'GROW'
+				? filtered.filter((item) => growFeedbacks.includes(item))
+				: filtered.filter((item) => glowFeedbacks.includes(item));
+	}
 
-  return !dumbBool ? (
-    <div className="category-view">
-      {filtered &&
-        filtered.map((feedback) => {
-          return <li key={feedback}>{feedback}</li>;
-        })}
-    </div>
-  ) : null;
+	return !dumbBool ? (
+		<div className='category-view'>
+			{filtered &&
+				filtered.map((feedback) => {
+					return <li key={feedback}>{feedback}</li>;
+				})}
+		</div>
+	) : null;
 }
